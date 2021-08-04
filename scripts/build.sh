b48b050bd7b1124279656dcac5a4fc9ab2646670
Binh Nguyen <thanhbinh01234@gmail.com>
2021-08-04 15:27:14 +0700

    Remove cjs-esm distinction

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,21 +3,16 @@
 # Remove prev build
 rm -rf lib
 
-# Build ESM package
-tsc -p tsconfig-esm.json && tsc-alias -f -p tsconfig-esm.json
-echo '{ "type": "module" }' >lib/esm/package.json
-
-# Build CJS package
-tsc -p tsconfig-cjs.json && tsc-alias -f -p tsconfig-cjs.json
+# Build package
+tsc && tsc-alias -f
 
 # Manually create ambient Typescript declaration
 # Normal declaration files are not compatible to exports map/dual type packages
-if [[ ! -d lib/types ]]; then mkdir lib/typings; fi
-npx tsc --emitDeclarationOnly --outFile lib/typings/index.d.ts
+npx tsc --declaration --emitDeclarationOnly --outFile lib/index.d.ts
 node node_modules/tfig/build/gift-cli.js \
-    -i lib/typings/index.d.ts \
+    -i lib/index.d.ts \
     -r index \
     -n "@presscommandz/js-data-structure" \
-    -o lib/typings/index.d.ts \
+    -o lib/index.d.ts \
     --config ./module-entries.json \
     -p false
