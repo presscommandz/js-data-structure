6f8201660cd60f11c9c1ba1d062097a2971e290a
Binh Nguyen <thanhbinh01234@gmail.com>
2021-08-07 18:13:58 +0700

    Add example for webpack

---
+++ example/webpack/src/index.js
@@ -0,0 +1,7 @@
+const { CoreGraphic } = require("@hlworld/js-data-structure")
+const { CGSize } = require("@hlworld/js-data-structure/core-graphic")
+
+const { CGPoint } = CoreGraphic
+
+console.log(new CGPoint(1, 1))
+console.log(new CGSize(1, 1))
