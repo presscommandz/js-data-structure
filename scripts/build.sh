e77ed7208f75448c421aeb2b883152f39257a9de
Binh Nguyen <thanhbinh01234@gmail.com>
2021-08-07 16:52:44 +0700

    Fix module import error in webpack

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-# Remove prev build
-rm -rf lib
+ttsc
 
-# Build package
-tsc && tsc-alias -f
+declare -a export_dirs=(
+    core-graphic
+)
 
-# Manually create ambient Typescript declaration
-# Normal declaration files are not compatible to exports map/dual type packages
-npx tsc --declaration --emitDeclarationOnly --outFile lib/index.d.ts
-node node_modules/tfig/build/gift-cli.js \
-    -i lib/index.d.ts \
-    -r index \
-    -n "@hlworld/js-data-structure" \
-    -o lib/index.d.ts \
-    --config ./module-entries.json \
-    -p false
+for dir in "${export_dirs[@]}"; do
+    cat <<EOF >"$dir/package.json"
+{
+    "main": "./index.js",
+    "types": "./index.d.ts"
+}
+EOF
+done
